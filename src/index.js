0c3a6f5790b77d9cf88905c9caf0447e55d1bfa5
Tarlan Isaev <tamerlanium@gmail.com>
2018-11-07 22:47:56 +0300

    Sorting

--- src/index.js
+++ src/index.js
@@ -30,19 +30,6 @@ var table_body_element = document.createElement('tbody');
 table_body_element.innerHTML = row_data;
 document.getElementById('table').appendChild(table_body_element);
 
-// const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
-// const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
-//   v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-// )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-
-// // do the work...
-// document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-//   const table = th.closest('table');
-//   Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-//     .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-//     .forEach(tr => table.appendChild(tr));
-// })));
 
 // var current_page = 1;
 // var records_per_page = 10;
